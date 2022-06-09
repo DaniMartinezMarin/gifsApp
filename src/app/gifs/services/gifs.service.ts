e32a3ce4cf5a5be79e60b235bcb7820849023451
Dani <dmartmar@mercadona.es>
2022-06-09 11:38:56 +0200

    feat: added http petition to get gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,15 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _historial: string[] = [];
+  private apiKey: string = 'CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5';
+
+  resultados: any [] = [];
 
   get historial(): string[] {
     return [...this._historial];
   }
 
-  constructor() {}
+  constructor(private ngHttpClient: HttpClient) {}
 
   buscarGifs(query: string = '') {
-
     query = query.toLowerCase();
 
     if (!this._historial.includes(query)) {
@@ -21,6 +24,11 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
 
-    console.log(this._historial);
+    this.ngHttpClient
+      .get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+      .subscribe((response: any) => {
+        console.log(response.data);
+        this.resultados = response.data;
+      });
   }
 }
